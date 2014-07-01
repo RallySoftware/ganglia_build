8cd2193a8545bee80a8bed4034898b040a7370d3
Aaron Nichols <anichols@trumped.org>
2014-07-01 15:55:08 -0600

    Updates to fix build - almost there

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#export DOCKER_HOST='tcp://bld-docker-02:4243'
 export DOCKER_HOST='tcp://bld-docker-02:4243'
 
-docker build --rm -t ganglia_centos_build centos/ 
+docker build -t ganglia_centos_build centos/ 
